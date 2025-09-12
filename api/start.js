75f1ba9cb50f2fff954da9753ad77b413b01c079
Taib3a <Taib3a@gmail.com>
2025-09-12 17:06:03 +0300

    Update start.js

--- api/start.js
+++ api/start.js
@@ -1,17 +1,26 @@
+import Aki from "aki-api";
 import axios from "axios";
 
 export default async function handler(req, res) {
   try {
-    // رابط صفحة البداية للعبة أكيناتور
-    const { data } = await axios.get("https://ar.akinator.com/");
+    // إعداد البروكسي
+    const proxy = "http://134.199.192.31:1337"; // اختر أي بروكسي من قائمتك
+    const instance = axios.create({ 
+      proxy: {
+        host: proxy.split(":")[1].replace("//", ""),
+        port: parseInt(proxy.split(":")[2])
+      },
+      timeout: 10000
+    });
 
+    // إنشاء جلسة Akinator مع البروكسي
+    const aki = new Aki("ar", instance);
+
+    await aki.start();
     res.status(200).json({
       success: true,
-      message: "تم بدء اللعبة بنجاح 🧞",
-      data: {
-        gameStatus: "start",
-        note: "استخدم ملفات step.js وanswer.js لإكمال اللعبة"
-      }
+      message: "تم بدء اللعبة 🧞",
+      question: aki.question
     });
   } catch (err) {
     res.status(500).json({
